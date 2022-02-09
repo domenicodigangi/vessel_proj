c50224557044849b4ec87b79c83d5dc31064734d
Domenico Di Gangi <digangidomenico@gmail.com>
2022-02-09 10:50:22 +0100

    update minor

--- scripts_and_notebooks/eda/ddg-eda-features.py
+++ scripts_and_notebooks/eda/ddg-eda-features.py
@@ -1,44 +1,15 @@
 #%%
-import fractions
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 import logging
 from pathlib import Path
-import time
-from sklearn.model_selection import train_test_split
-from sklearn import preprocessing
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.svm import SVR
-from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import Ridge
-import sklearn
-
-from sklearn.svm import SVR
-from sklearn.feature_selection import SelectFromModel
-from sklearn.pipeline import Pipeline
-from sklearn.feature_selection import f_regression, mutual_info_regression
-from sklearn.feature_selection import SelectKBest
-from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
-
-from sklearn.model_selection import train_test_split
-from sklearn.utils.class_weight import compute_class_weight
 
 from sklearn.exceptions import ConvergenceWarning
 import warnings
 
-from torch import frac_
 warnings.filterwarnings("ignore", category=ConvergenceWarning)
-from vessel_proj.data import get_one_file_from_artifact, get_project_name, get_wandb_root_path
-import wandb
-from sklearn.inspection import permutation_importance
-
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import make_scorer
-from sklearn.model_selection import cross_val_score
-from xgboost import XGBRegressor
 from vessel_proj.data import get_latest_port_data
-from sklearn.cluster import KMeans
 import seaborn as sns
 sns.set_theme(style="darkgrid")
 
@@ -47,11 +18,15 @@ from vessel_proj.task.classification_task_pipeline_ports_centr import encode_fea
 
 data = get_latest_port_data()
 
-df = encode_features.fn(data)["features"]
+df = data["features"]
+df_enc = encode_features.fn(data, feat_names_non_cat=["TIDE_RANGE", "LATITUDE", "LONGITUDE"], cols_to_drop=["PORT_NAME", "REGION_NO", "PUB"],)["features"]
 
+df.shape
 #%% Missing
 fract_miss = df.isnull().mean().sort_values(ascending=False)
 
+df.columns[df.dtypes == np.float64]
+df["TIDE_RANGE"].hist()
 
 fig, ax = plt.subplots(figsize=(10,12))
 g = sns.barplot(y=fract_miss.index, x=fract_miss.values, ax=ax)
@@ -59,9 +34,9 @@ g.tick_params(axis='x', rotation=90)
 g.set_xlabel("Fraction of Missing Values")
 #%%
 
-df = drop_missing_cols.fn({"features": df})["features"]
+df_enc = drop_missing_cols.fn({"features": df_enc})["features"]
 
 
-df.info()
+df_enc.info()
 
 # %%
