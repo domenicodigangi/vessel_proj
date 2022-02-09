f402bc92a3410dcc075dc6cad7df20ec3c1d132c
Domenico Di Gangi <digangidomenico@gmail.com>
2022-02-09 17:43:09 +0100

    update minor

--- scripts_and_notebooks/eda/ddg-eda-centralities.py
+++ scripts_and_notebooks/eda/ddg-eda-centralities.py
@@ -2,39 +2,21 @@
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
-import logging
-from pathlib import Path
-import time
 from sklearn.model_selection import train_test_split
-from sklearn import preprocessing
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.svm import SVR
-from sklearn.linear_model import LinearRegression
-from sklearn.linear_model import Ridge
 import sklearn
 
 from sklearn.svm import SVR
-from sklearn.feature_selection import SelectFromModel
-from sklearn.pipeline import Pipeline
-from sklearn.feature_selection import f_regression, mutual_info_regression
-from sklearn.feature_selection import SelectKBest
 from sklearn.preprocessing import OneHotEncoder, OrdinalEncoder
 
 from sklearn.model_selection import train_test_split
-from sklearn.utils.class_weight import compute_class_weight
 
 from sklearn.exceptions import ConvergenceWarning
 import warnings
 warnings.filterwarnings("ignore", category=ConvergenceWarning)
-from vessel_proj.data import get_one_file_from_artifact, get_project_name, get_wandb_root_path
-import wandb
-from sklearn.inspection import permutation_importance
-
-from sklearn.metrics import accuracy_score
 from sklearn.metrics import make_scorer
-from sklearn.model_selection import cross_val_score
 from xgboost import XGBRegressor
-from vessel_proj.data import get_latest_port_data
+from vessel_proj.data import get_latest_port_data, get_project_name, save_parquet_and_wandb_log, get_project_name
+from vessel_proj.task.classification_task_pipeline_ports_centr import add_avg_centr
 from sklearn.cluster import KMeans
 import seaborn as sns
 sns.set_theme(style="darkgrid")
@@ -43,8 +25,9 @@ sns.set_theme(style="darkgrid")
 data = get_latest_port_data()
 df_feat = data["features"]
 df_centr = data["centralities"]
+df_centr = add_avg_centr.fn(data)["centralities"]
 
-
+save_parquet_and_wandb_log(df_centr, "centralities-ports", "interim", project=get_project_name())
 
 #%% Scatter
 df = df_centr.drop(columns=["page_rank_w_trips", "centr_eig_w_trips", "centr_eig_bin", "centr_eig_w_log_trips"])
