1ca71546688f24f6bbdd0e8d326bf7f9e63ac00f
Domenico Di Gangi <digangidomenico@gmail.com>
2022-09-10 12:01:13 +0200

    wip

--- scripts_and_notebooks/gnn/predict_port_feat.py
+++ scripts_and_notebooks/gnn/predict_port_feat.py
@@ -1,11 +1,14 @@
 # %%
+%load_ext dotenv
+%dotenv /home/digan/cnr/vessel_proj/vessel_proj_secrets.env
+
 from torch_geometric.nn import GCNConv
 import torch.nn.functional as F
 import torch
 from vessel_proj.preprocess_data.gnn import get_graph_from_saved_data, get_train_mask
 import mlflow
 from vessel_proj.ds_utils import set_mlflow
-from vessel_proj.ds_utils.torch.opt import optimize_torch_obj_fun
+from torch.utils.tensorboard import SummaryWriter
 
 
 EXPERIMENT_VERSION = "test"
@@ -45,8 +48,6 @@ set_mlflow()
 
 experiment_name = f"predict_node_feat_{EXPERIMENT_VERSION}"
 mlflow.set_experiment(experiment_name)
-
-# Get Experiment Details
 experiment = mlflow.get_experiment_by_name(experiment_name)
 
 with mlflow.start_run(experiment_id=experiment.experiment_id) as run:
@@ -54,7 +55,7 @@ with mlflow.start_run(experiment_id=experiment.experiment_id) as run:
     graph = get_graph_from_saved_data()
     graph.num_classes = graph.y.unique().shape[0]
     graph.x
-    n = 1000
+    n_train = 1000
 
     graph.train_mask, graph.test_mask = get_train_mask(graph.x.shape[0])
 
@@ -63,141 +64,35 @@ with mlflow.start_run(experiment_id=experiment.experiment_id) as run:
     data = graph.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
 
-    model.train()
-    for epoch in range(1000):
-        optimizer.zero_grad()
-        out = model(data)
-        loss = F.nll_loss(out[data.train_mask, :], data.y[data.train_mask])
-        loss.backward()
-        optimizer.step()
-        acc = get_accuracy(model, data)
-        print(f"Accuracy: {acc:.4f}")
-# %%
-# %%
-with mlflow.start_run(run_id=parent_run.info.run_id, nested=True):
-    with mlflow.start_run(experiment_id=parent_run.info.experiment_id, nested=True):
 
-        logger.info(run_par_dict)
+    with tempfile.TemporaryDirectory() as tmpdirname:
 
-        # save files in temp folder, then log them as artifacts in mlflow and delete temp fold
+        tmp_path = Path(tmpdirname)
+        tb_fold = tmp_path / "tb_logs"
+        tb_fold.mkdir(exist_ok=True)
+        tb_log_str = f"saving to tb logs to {tb_fold}"
+        writer = SummaryWriter(str(tb_fold))
 
-        with tempfile.TemporaryDirectory() as tmpdirname:
+        mlflow.log_params(parent_runs_par)
+        mlflow.log_metrics()
 
-            # set artifacts folders and subfolders
-            tmp_path = Path(tmpdirname)
-            tb_fold = tmp_path / "tb_logs"
-            tb_fold.mkdir(exist_ok=True)
+        torch.save(run_data_dict["Y_reference"], dgp_fold / "Y_reference.pt")
+                            
+        # log all files and sub-folders in temp fold as artifacts
+        mlflow.log_artifacts(tmp_path)
 
-            mlflow.log_params(parent_runs_par)
-            mlflow.log_params(
-                {
-                    f"dgp_{bin_or_w}_{key}": val
-                    for key, val in run_par_dict[f"dgp_par_{bin_or_w}"].items()
-                }
-            )
+        model.train()
+        for epoch in range(n_train):
+            optimizer.zero_grad()
+            out = model(data)
+            loss = F.nll_loss(out[data.train_mask, :], data.y[data.train_mask])
+            loss.backward()
+            optimizer.step()
+            valid_accuracy = get_accuracy(model, data)
 
-            torch.save(run_data_dict["Y_reference"], dgp_fold / "Y_reference.pt")
-            torch.save(
-                (mod_dgp.get_Y_T_to_save(), mod_dgp.X_T),
-                dgp_fold / "obs_T_dgp.pt",
-            )
-            mod_dgp.save_parameters(save_path=dgp_fold)
-            if mod_dgp.phi_T is not None:
-                mlflow.log_figure(
-                    mod_dgp.plot_phi_T()[0], f"fig/{bin_or_w}_dgp_all.png"
-                )
 
-                # estimate models and log parameters and hpar optimization
-                filt_models = get_filt_mod(
-                    bin_or_w,
-                    mod_dgp.Y_T,
-                    mod_dgp.X_T,
-                    run_par_dict[f"filt_par_{bin_or_w}"],
-                )
+            writer.add_scalar("Loss/value", loss.item(), epoch)
+            writer.add_scalar("Valid Accuracy", valid_accuracy, epoch)
+            writer.add_scalar("Loss/grad_norm", grad_norm, epoch)
+            writer.add_scalar("Loss/grad_max", grad_norm, epoch)
 
-                # k_filt, mod = list(filt_models.items())[0]
-                for k_filt, mod_filt in filt_models.items():
-
-                    _, h_par_opt, stats_opt = mod_filt.estimate(tb_save_fold=tb_fold)
-
-                    mlflow.log_params(
-                        {
-                            f"filt_{bin_or_w}_{k_filt}_{key}": val
-                            for key, val in h_par_opt.items()
-                        }
-                    )
-                    mlflow.log_metrics(
-                        {
-                            f"filt_{bin_or_w}_{k_filt}_{key}": val
-                            for key, val in stats_opt.items()
-                        }
-                    )
-                    mlflow.log_params(
-                        {
-                            f"filt_{bin_or_w}_{k_filt}_{key}": val
-                            for key, val in mod_filt.get_info_dict().items()
-                            if key not in h_par_opt.keys()
-                        }
-                    )
-
-                    mod_filt.save_parameters(save_path=tmp_path)
-
-                    # compute mse for each model and log it
-                    nodes_to_exclude = mod_dgp.get_inds_inactive_nodes()
-
-                    mse_dict = filt_err(
-                        mod_dgp, mod_filt, suffix=k_filt, prefix=bin_or_w
-                    )
-                    mlflow.log_metrics(mse_dict)
-                    logger.warning(mse_dict)
-
-                    # mlflow.log_metrics({f"filt_{bin_or_w}_{k_filt}_{key}": v for key, v in mod_filt.out_of_sample_eval().items()})
-
-                    # log plots that can be useful for quick visual diagnostic
-                    if mod_filt.phi_T is not None:
-                        mlflow.log_figure(
-                            mod_filt.plot_phi_T()[0],
-                            f"fig/{bin_or_w}_{k_filt}_filt_all.png",
-                        )
-                    i_plot = torch.where(~splitVec(nodes_to_exclude)[0])[0][0]
-
-                    if mod_filt.phi_T is not None:
-                        if mod_dgp.phi_T is not None:
-                            fig_ax = mod_dgp.plot_phi_T(i=i_plot)
-                        else:
-                            fig_ax = None
-
-                        mlflow.log_figure(
-                            mod_filt.plot_phi_T(i=i_plot, fig_ax=fig_ax)[0],
-                            f"fig/{bin_or_w}_{k_filt}_filt_phi_ind_{i_plot}.png",
-                        )
-
-                    if mod_dgp.X_T is not None:
-                        avg_beta_dict = {
-                            f"{bin_or_w}_{k}_dgp": v
-                            for k, v in mod_dgp.get_avg_beta_dict().items()
-                        }
-                        mlflow.log_metrics(avg_beta_dict)
-                        mlflow.log_metric(
-                            f"{bin_or_w}_avg_beta_dgp",
-                            np.mean(list(avg_beta_dict.values())),
-                        )
-
-                        fig = plt.figure()
-                        plt.plot(mod_dgp.X_T[0, 0, :, :].T, figure=fig)
-                        mlflow.log_figure(fig, f"fig/{bin_or_w}_X_0_0_T.png")
-
-                    if mod_dgp.any_beta_tv():
-                        plot_dgp_fig_ax = mod_dgp.plot_beta_T()
-                        mlflow.log_figure(
-                            plot_dgp_fig_ax[0], f"fig/{bin_or_w}_sd_filt_beta_T.png"
-                        )
-                    if mod_filt.beta_T is not None:
-                        if mod_filt.any_beta_tv():
-                            mlflow.log_figure(
-                                mod_filt.plot_beta_T(fig_ax=plot_dgp_fig_ax)[0],
-                                f"fig/{bin_or_w}_{k_filt}_filt_beta_T.png",
-                            )
-
-            # log all files and sub-folders in temp fold as artifacts
-            mlflow.log_artifacts(tmp_path)
